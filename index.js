0b352ccdc6b8a6608b74d37fe8413527d73f56b1
Govindan Vedanarayanan <vamsivedanarayanan@gmail.com>
2017-03-29 19:51:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-var https = require('https');
+var helper = require('./helper');
 var placesAPI = 'AIzaSyCjuvR8Wfdzg2e3EVmU30Lun8hcmxyeSLo';
+var directionsAPI = 'AIzaSyDBuY63041R_cpalQykTqpuFRvm_zz1EKA';
+var geocodingAPI = 'AIzaSyDWk0jqpQ0UTAMIO9zKw6O8UrSHGfwjpts';
+var ipinfoAPI = '06fa9a46e07d49f65936a81ee009395ffc07bd437c9f71ee74371135406605ca';
 
 const restService = express();
 
@@ -13,44 +16,40 @@ restService.use(bodyParser.urlencoded({
 
 restService.use(bodyParser.json());
 
+// My Location: 40.48850079103278, -74.43782866001129
 var speech = '';
-var lat = 40.48850079103278;
-var lng = -74.43782866001129;
+var lat;
+var lng;
+
+helper.ipInfo();
 
 restService.post('/bot', function(req, res) {
     var json = req.body.result;
     console.log("1");
-    if (json.action === 'type.name') type_name(json, res);
-    else if (json.action === 'type.info') type_info(json, res);
-    else if (json.action === 'text.search') text_search(json, res);
-    else if (json.action === 'user.location') user_location(res);
-    else
+    var action = json.action;
+    switch (action)
     {
-        var speech = "else part was called.";
-        return res.json({
-        speech: speech,
-        displayText: speech,
-        source: 'no source'
-        });
+        case 'type.name': 
+            type_name(json, res);
+            break;
+        case 'type.info': 
+            type_info(json, res);
+            break;
+        case 'text.search': 
+            text_search(json, res);
+            break;
+        case 'user.location': 
+            user_location(res);
+            break;
+        default:
+            console.log("This will never be the case!");
     }
 });
 
-function httpsGet(url, callback) {
-    https.get(url, function(res) {
-        var body = '';
-        res.on('data', function(data) {
-            body += data;
-        });
-        res.on('end', function() {
-            callback(body);
-        });
-    });
-};
-
-function type_name(json, res)
+function type_name(json, res) 
 {
     var url;
-    var type = json.parameters.type; //update: removed // remove [0] if you uncheck 'IS LIST' in the intent
+    var type = json.parameters.type;
     var withRadius = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+lat+','+lng+'&radius=5000&rankby=prominence&type='+type+'&key='+placesAPI;
     var withoutRadius = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+lat+','+lng+'&rankby=distance&type='+type+'&key='+placesAPI;
     if (json.parameters.rankby == 'prominence')
@@ -63,62 +62,46 @@ function type_name(json, res)
         speech = "Based on distance: ";
         url = withoutRadius;
     }
-
-    /*
-    if (json.parameters.rankby.length == 2) 
-    {
-        speech = "Based on distance: ";
-        url = withoutRadius;
-    }
-    else 
-    {
-        if (json.parameters.rankby == 'prominence') { speech = "Based on prominence: "; url = withRadius; }
-        else { speech = "Based on distance: "; url = withoutRadius; }
-    }
-    */
-    httpsGet(url, function(response) {
+    helper.httpsGet(url, function(response) {
         var result = JSON.parse(response);
         for (var i = 0; i < 5 && i < result.results.length; i++)
             speech += "\n" + (i + 1) + ". " + result.results[i].name + " | " + result.results[i].vicinity;
-        return res.json({
-        speech: speech,
-        displayText: speech,
-        source: 'googleapis'
-        });
+        helper.returnSpeech(res, speech);
     });
 };
 
-function type_info(json, res)
+function type_info(json, res) 
 {
     var keyword = json.parameters.type_original;
     keyword.replace(/ /g, "+").replace(/\?/g, "");
     var url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+lat+','+lng+'&rankby=distance&keyword='+keyword+'&key='+placesAPI;
-    httpsGet(url, function(response) {
+    helper.httpsGet(url, function(response) {
         var result = JSON.parse(response);
         var placeId = result.results[0].place_id;
         url = 'https://maps.googleapis.com/maps/api/place/details/json?placeid='+placeId+'&key='+placesAPI;
-        httpsGet(url, function(response) {
+        helper.httpsGet(url, function(response) {
             result = JSON.parse(response);
             var name = result.result.name;
+            // json.parameters.type_original + " - " + 
             if (json.parameters.json_key === 'rating')
             {
                 var rating = result.result.rating;
-                speech = "Rating of nearest " + json.parameters.type_original + " - " + name + ": " + rating;
+                speech = "Rating of " + name + ": " + rating;
             }
             else if (json.parameters.json_key === 'website')
             {
                 var website = result.result.website;
-                speech = "Website of nearest " + json.parameters.type_original + " - " + name + ": " + website;
+                speech = "Website of " + name + ": " + website;
             }
             else if (json.parameters.json_key === 'international_phone_number')
             {
                 var international_phone_number = result.result.international_phone_number;
-                speech = "Contact of nearest " + json.parameters.type_original + " - " + name  + ": " + international_phone_number;
+                speech = "Contact of " + name  + ": " + international_phone_number;
             }
             else if (json.parameters.json_key === 'formatted_address')
             {
                 var formatted_address = result.result.formatted_address;
-                speech = "Address of nearest " + json.parameters.type_original + " - " + name + ": " + formatted_address;
+                speech = "Address of " + name + ": " + formatted_address;
             }
             else if (json.parameters.json_key === 'opening_hours')
             {
@@ -127,7 +110,7 @@ function type_info(json, res)
                 {
                     var opening_hours = result.result.opening_hours.open_now;
                     if (opening_hours) speech = json.parameters.type_original + " is open now."; // replace()
-                    else speech = "Nearest " + name /*json.parameters.type_original*/ + " is closed now.";
+                    else speech = name /*json.parameters.type_original*/ + " is closed now.";
                 }
             }
             else if (json.parameters.json_key === 'reviews')
@@ -139,67 +122,97 @@ function type_info(json, res)
                         reviews += "\n" + (i + 1) + ". Name: " + result.result.reviews[i].author_name + " | Rating: " + result.result.reviews[i].aspects[0].rating;
                         reviews += "\nReview: " + result.result.reviews[i].text;                
                     }
-                speech = "Reviews of nearest " + json.parameters.type_original + " - " + name + ":";
+                speech = "Reviews of " + name + ":";
                 speech += reviews;
             }
             // var photos;
-            return res.json({
-            speech: speech,
-            displayText: speech,
-            source: 'googleapis'
-            });
+            helper.returnSpeech(res, speech);
         });
     });
 }
 
-function text_search(json, res) {
+function text_search(json, res) 
+{
     var phrase = json.parameters.phrase;
     phrase.replace(/ /g, "+").replace(/\?/g, "");
-    speech = phrase + ":\n";
+    speech = "I hope this is helpful:\n";
     var url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query='+phrase+'&key='+placesAPI;
-    httpsGet(url, function(response) {
+    helper.httpsGet(url, function(response) {
         var result = JSON.parse(response);
         for (var i = 0; i < 5 && i < result.results.length; i++)
             speech += "\n" + (i + 1) + ". " + result.results[i].name + " | " + result.results[i].formatted_address;
-        return res.json({
-        speech: speech,
-        displayText: speech,
-        source: 'googleapis'
-        });
+        helper.returnSpeech(res, speech);
+    });
+}
+
+function html_directions_driving(json, res) 
+{
+    var source = json.parameters.directions.source;
+    var destination = json.parameters.directions.destination;
+    speech = "Directions provided by Google:\n\n";
+    var url = 'https://maps.googleapis.com/maps/api/directions/json?origin='+source+'&destination='+destination+'&key='+directionsAPI;
+    helper.httpsGet(url, function(response) {
+        var result = JSON.parse(response);
+        var localJson = result.routes[0].legs[0].steps;
+        for (var i = 0; i < localJson.length; i++) 
+        {
+            var index = i + 1;
+            var inst = (localJson[i].html_instructions).replace(/<b>/g, "").replace(/<\/b>/g, "").replace(/<\/div>/g, "").replace(/<div style="font-size:0.9em">/g, ". ");
+            speech += index + ". " + inst + " for " + localJson[i].distance.text + ".\n";
+        }
+        speech += "The total travel time is " + result.routes[0].legs[0].duration.text;
+        helper.returnSpeech(res, speech);
+    });
+}
+
+function html_directions_transit(json, res) 
+{
+    var source = json.parameters.directions.source;
+    var destination = json.parameters.directions.destination;
+    var url = 'https://maps.googleapis.com/maps/api/directions/json?mode=transit&transit_mode=train&origin='+source+'&destination='+destination+'&key='+directionsAPI;
+    helper.httpsGet(url, function(response) {
+        var result = JSON.parse(response);
+        var speech = "Directions provided by Google\n\n";
+        var jsonOne = result.routes[0].legs[0].steps;
+        for (var i = 0; i < jsonOne.length; i++) 
+        {
+            var index = i + 1;
+            speech += index + ". " + jsonOne[i].html_instructions + "\n";
+            if (jsonOne[i].travel_mode === "WALKING") // if ((jsonOne[i].html_instructions).search("Walk") != -1)
+            {
+                var jsonTwo = jsonOne[i].steps;
+                for (var j = 0; j < jsonTwo.length; j++)
+                {
+                    if (jsonTwo[j].html_instructions === undefined) continue;
+                    var inst = (jsonTwo[j].html_instructions).replace(/<b>/g, "").replace(/<\/b>/g, "").replace(/<\/div>/g, "").replace(/<div style="font-size:0.9em">/g, ". ");
+                    speech += "-> " + inst + " (" + jsonTwo[j].distance.text + ")\n";
+                }
+            }
+            else if (jsonOne[i].travel_mode === "TRANSIT")
+            {
+                var jsonTwo = jsonOne[i].transit_details;
+                speech += "-> Line name: " + jsonTwo.line.name + "\n";
+                speech += "-> Number of stops: " + jsonTwo.num_stops + "\n";
+                speech += "-> Departure Stop: " + jsonTwo.departure_stop.name + "\n";
+                speech += "-> Departure Time: " + jsonTwo.departure_time.text + "\n";
+                speech += "-> Arrival Stop: " + jsonTwo.arrival_stop.name + "\n";
+                speech += "-> Arrival Time: " + jsonTwo.arrival_time.text + "\n";
+            }
+        }
+        speech += "The total travel time is " + result.routes[0].legs[0].duration.text;
+        helper.returnSpeech(res, speech);
     });
 }
 
 function user_location(res)
 {
-    var lat, lng;
-    var options = {
-        "method": "POST",
-        "hostname": "www.googleapis.com",
-        "port": null,
-        "path": "/geolocation/v1/geolocate?key=AIzaSyDv2mrisGGS-aYNzuPOu8rxe4U6Pa132qo"
-        };
-    var req = https.request(options, function (response) {
-        //var chunks = '';
-        var chunks = [];
-        response.on("data", function (chunk) {
-            //chunks += chunk;
-            chunks.push(chunk);
-        });
-        response.on("end", function () {
-            var body = Buffer.concat(chunks);
-            speech = body.toString();
-            /*var result = JSON.parse(chunks);
-            lat = result.location.lat;
-            lng = result.location.lng;
-            var speech = "Latitude: " + lat + " | " + "Longitude: " + lng;*/
-            return res.json({
-            speech: speech,
-            displayText: speech,
-            source: 'googleapis'
-            });
-        });
+    speech = "You are currently here:\n";
+    var url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+lat+','+lng+'&key='+geocodingAPI;
+    helper.httpsGet(url, function(response) {
+        var result = JSON.parse(response);
+        speech += result.results[0].formatted_address;
+        helper.returnSpeech(res, speech);
     });
-    req.end();
 }
 
 restService.listen((process.env.PORT || 8000), function() {
