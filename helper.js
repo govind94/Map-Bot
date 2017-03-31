adcfa0a905b1b13b352d6a306ae246b9aa5fdd2c
Govindan Vedanarayanan <vamsivedanarayanan@gmail.com>
2017-03-31 19:18:51 -0400

    Update helper.js

--- helper.js
+++ helper.js
@@ -1,16 +1,5 @@
 var https = require('https');
 
-function ipInfo()
-{
-    // public IP
-    var url = 'https://api.ipinfodb.com/v3/ip-city/?format=json&ip=69.116.29.239&key='+ipinfoAPI;
-    helper.httpsGet(url, function(response) {
-        var result = JSON.parse(response);
-        lat = result.latitude - 0.051;
-        lng = result.longitude - (-0.0282);
-    });
-}
-
 function httpsGet(url, callback) 
 {
     https.get(url, function(res) {
@@ -33,6 +22,5 @@ function returnSpeech(res, speech)
     });
 }
 
-exports.ipInfo = ipInfo;
 exports.httpsGet = httpsGet;
 exports.returnSpeech = returnSpeech;
