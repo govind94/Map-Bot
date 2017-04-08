0ea61ba6cf86244cfcb6bfa3442aaf5519e4d2f4
Govindan Vedanarayanan <vamsivedanarayanan@gmail.com>
2017-04-07 21:12:38 -0400

    Update helper.js

--- helper.js
+++ helper.js
@@ -32,6 +32,15 @@ function returnSpeechWithData(res, speech, data)
     });
 }
 
+function type_name_helper(result, res, head)
+{
+    speech = (result.results.length < 3) ? ("I could find only " + result.results.length + ":\n\n") : head;
+    for (var i = 0; i < 5 && i < result.results.length; i++)
+        speech += "\n\n" + (i + 1) + ". " + result.results[i].name + " | " + result.results[i].vicinity;
+    returnSpeech(res, speech);
+}
+
 exports.httpsGet = httpsGet;
 exports.returnSpeech = returnSpeech;
 exports.returnSpeechWithData = returnSpeechWithData;
+exports.type_name_helper = type_name_helper;
