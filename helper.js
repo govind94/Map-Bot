0ed0ff2a146a3c92af28b7ddd807f4d6062f3ebd
Govindan Vedanarayanan <vamsivedanarayanan@gmail.com>
2017-04-15 15:59:06 -0400

    Update helper.js

--- helper.js
+++ helper.js
@@ -34,9 +34,12 @@ function returnSpeechWithData(res, speech, data)
 
 function type_name_helper(result, res, head)
 {
-    speech = (result.results.length < 3) ? ("I could find only " + result.results.length + ":\n\n") : head;
+    var len = result.results.length;
+    if (len == 0) helper.returnSpeech(res, "I couldn't find what you were looking for. As a reminder,  I am searching for places based on your location only. To look up places far away, type \"Specific requests\" or \"Info about places\".");
+    speech = (len < 3) ? ("I could find only " + (len == 1 ? "one" : "two") + ":\n\n") : head;
     for (var i = 0; i < 5 && i < result.results.length; i++)
         speech += "\n\n" + (i + 1) + ". " + result.results[i].name + " | " + result.results[i].vicinity;
+    speech += "\n\nDo you want to go to any one of the above places? Just copy and paste the address associated with the place.";
     returnSpeech(res, speech);
 }
 
